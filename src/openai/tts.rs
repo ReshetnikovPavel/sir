0867a1b2891bd5b00dfa950c1992d25c22795c51
Pavel Reshetnikov <pavelresh@yandex.ru>
2025-09-13 01:29:12 +0500

    refactor: tts

--- src/openai/tts.rs
+++ src/openai/tts.rs
@@ -1,3 +1,4 @@
+use async_openai::error::OpenAIError;
 use reqwest::Client;
 
 use crate::openai::config::TtsConfig;
@@ -8,7 +9,7 @@ pub struct TextToSpeech {
 }
 
 impl TextToSpeech {
-    pub async fn speech(&self, input: &str) -> Result<Vec<u8>, anyhow::Error> {
+    pub async fn speech(&self, input: &str) -> Result<Vec<u8>, OpenAIError> {
         let url = self.config.api_base.clone() + "/audio/speech";
         let json = serde_json::json!({
             "input": input,
@@ -16,7 +17,15 @@ impl TextToSpeech {
             "voice": self.config.voice,
             "response_format": "wav"
         });
-        let response = self.client.post(url).body(json.to_string()).send().await?;
+
+        let response = self
+            .client
+            .post(url)
+            .body(json.to_string())
+            .send()
+            .await?
+            .error_for_status()?;
+
         let content = response.bytes().await?.to_vec();
 
         Ok(content)
